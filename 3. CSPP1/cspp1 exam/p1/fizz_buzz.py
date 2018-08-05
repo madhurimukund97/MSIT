229886cc3fdb00813bb2d7a46097628b0214f26d
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-05 12:34:19 +0530

    " -> 4 of 7 passed. pylint: 10.00/10 "

--- 3. CSPP1/cspp1 exam/p1/fizz_buzz.py
+++ 3. CSPP1/cspp1 exam/p1/fizz_buzz.py
@@ -11,8 +11,8 @@ def main():
     num = int(input())
     for i in range(1, num+1, 1):
         if i%3 == 0 and i%5 == 0:
-            print("Fizz")
-            print("Buzz")
+            print("FizzBuzz")
+            # print("Buzz")
         elif i%3 == 0:
             print("Fizz")
         elif i%5 == 0:
