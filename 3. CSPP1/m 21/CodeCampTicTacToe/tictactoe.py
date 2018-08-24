5446a39d6f6fca3586cfe397b68272b91aadf991
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-24 13:39:56 +0530

    " -> 14 of 14 passed. pylint: 10.00/10 "

--- 3. CSPP1/m 21/CodeCampTicTacToe/tictactoe.py
+++ 3. CSPP1/m 21/CodeCampTicTacToe/tictactoe.py
@@ -1,4 +1,6 @@
+'''game'''
 def tic_tac_toe(mat):
+    '''tic tac toe game'''
     win = []
     for row in mat:
         if row[0] == row[1] == row[2]:
@@ -10,7 +12,7 @@ def tic_tac_toe(mat):
         win.append(mat[0][0])
     if mat[2][0] == mat[1][1] == mat[0][2]:
         win.append(mat[0][2])
-    if len(win) == 0:
+    if len(win) is 0:
         print('draw')
         return None
     if len(win) == 1:
@@ -23,6 +25,7 @@ def tic_tac_toe(mat):
         print("invalid game")
         return None
 def main():
+    '''main'''
     list1 = []
     for _ in range(0, 3):
         cols = input().split(' ')
