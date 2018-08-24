11a9ffb19537797af24064e60854ab9542775ca7
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-24 13:36:59 +0530

    " -> 14 of 14 passed. pylint: 8.67/10 "

--- 3. CSPP1/m 21/CodeCampTicTacToe/tictactoe.py
+++ 3. CSPP1/m 21/CodeCampTicTacToe/tictactoe.py
@@ -1,34 +1,32 @@
 def tic_tac_toe(mat):
-	win = []
-	for row in mat:
-		if row[0] == row[1] == row[2]:
-			win.append(row[0])
-	for ind in range(0, 3):
-		if mat[0][ind] == mat[1][ind] == mat[2][ind]:
-			win.append(mat[0][ind])
-	if mat[0][0] == mat[1][1] == mat[2][2]:
-		win.append(mat[0][0])
-	if mat[2][0] == mat[1][1] == mat[0][2]:
-		win.append(mat[0][2])
-	if len(win) == 0:
-		print('draw')
-		return None
-	if len(win) == 1:
-		if win[0] == 'x' or win[0] == 'o':
-			print(win[0])
-		else:
-			print("invalid input")
-		return win[0]
-	else:
-		print("invalid game")
-		return None
+    win = []
+    for row in mat:
+        if row[0] == row[1] == row[2]:
+            win.append(row[0])
+    for ind in range(0, 3):
+        if mat[0][ind] == mat[1][ind] == mat[2][ind]:
+            win.append(mat[0][ind])
+    if mat[0][0] == mat[1][1] == mat[2][2]:
+        win.append(mat[0][0])
+    if mat[2][0] == mat[1][1] == mat[0][2]:
+        win.append(mat[0][2])
+    if len(win) == 0:
+        print('draw')
+        return None
+    if len(win) == 1:
+        if win[0] == 'x' or win[0] == 'o':
+            print(win[0])
+        else:
+            print("invalid input")
+        return win[0]
+    else:
+        print("invalid game")
+        return None
 def main():
-	list1 = []
-	for _ in range(0, 3):
-		cols = input().split(' ')
-		list1.append(cols)
-	tic_tac_toe(list1)
+    list1 = []
+    for _ in range(0, 3):
+        cols = input().split(' ')
+        list1.append(cols)
+    tic_tac_toe(list1)
 if __name__ == '__main__':
-	main()
-
-	 	
+    main()
