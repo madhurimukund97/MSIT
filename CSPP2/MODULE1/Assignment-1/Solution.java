60df62039b9a8a3def051bd0790f3459184feb39
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-08-27 18:15:07 +0530

    Added cspp2 folder in MSIT

---
+++ CSPP2/MODULE1/Assignment-1/Solution.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		String name = s.nextLine();
+		int number = s.nextInt();
+		System.out.println("Name : " + name + "Roll Number : "+ number);
+		// System.out.println("Roll Number : "+number);
+	}
+}
---
+++ CSPP2/MODULE1/degrees_to_fahrenheit.java
@@ -0,0 +1,9 @@
+class degrees_to_fahrenheit{
+	public static void main(String[] args) {
+		double cent = 70;
+		double fahr;
+		fahr = 9*cent/5-32;
+		System.out.println(fahr);
+
+	}
+}
